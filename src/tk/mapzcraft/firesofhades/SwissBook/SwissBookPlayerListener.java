acc60baad90b508503d43c5d85c0b36d468f444c
firesofhades <firesofhades1@hotmail.com>
2013-09-06 14:57:07 +0200

    updated to v1.1

--- src/tk/mapzcraft/firesofhades/SwissBook/SwissBookPlayerListener.java
+++ src/tk/mapzcraft/firesofhades/SwissBook/SwissBookPlayerListener.java
@@ -1,12 +1,16 @@
 package tk.mapzcraft.firesofhades.SwissBook;
 
+import org.bukkit.Material;
 import org.bukkit.block.Chest;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractEntityEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerPickupItemEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.BookMeta;
 import org.bukkit.util.BlockVector;
 
 public class SwissBookPlayerListener implements Listener {
@@ -30,8 +34,42 @@ public class SwissBookPlayerListener implements Listener {
 
 				i.addItem(manual);
 				event.getPlayer().updateInventory();
-				event.getPlayer().sendMessage("You have been given a manual, READ IT!!!");
+				event.getPlayer().sendMessage(
+						"You have been given a manual, READ IT!!!");
 			}
 		}
+	}
+
+	@EventHandler(priority = EventPriority.NORMAL)
+	public void onPickupItem(PlayerPickupItemEvent event) {
+		if (((BookMeta) event.getItem().getItemStack().getItemMeta())
+				.getTitle().equals(
+						((BookMeta) plugin.manual.getItemStack("Manual")
+								.getItemMeta()).getTitle())) {
+			event.setCancelled(true);
+		}
+	}
+
+	@EventHandler(priority = EventPriority.NORMAL)
+	public void onEntityInteract(PlayerInteractEntityEvent event) {
+
+		int x = 0;
+		ItemStack[] invarray = event.getPlayer().getInventory().getContents();
+		while (invarray.length > x) {
+			if (invarray[x] != null) {
+				if (invarray[x].getType().equals(Material.WRITTEN_BOOK)) {
+					if (((BookMeta) invarray[x].getItemMeta()).getTitle()
+							.equals(((BookMeta) plugin.manual.getItemStack(
+									"Manual").getItemMeta()).getTitle())) {
+						event.setCancelled(true);
+						event.getPlayer()
+								.sendMessage(
+										"you can not trade with villagers while carrying a manual");
+					}
+				}
+			}
+			x = x + 1;
+		}
+
 	}
 }
